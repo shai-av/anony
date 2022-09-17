f98ad5fec1b64fc4cdf9971e762c3b40b8b86238
Shai <shay4447@gmail.com>
2022-09-17 18:54:53 +0300

    main-app layout

--- src/app/pages/chat-room/chat-room.component.ts
+++ src/app/pages/chat-room/chat-room.component.ts
@@ -1,15 +1,18 @@
 import { Component, OnInit } from '@angular/core';
 
 @Component({
-  selector: 'app-chat-room',
+  selector: 'chat-room',
   templateUrl: './chat-room.component.html',
   styleUrls: ['./chat-room.component.scss']
 })
 export class ChatRoomComponent implements OnInit {
-
+  
   constructor() { }
-
+  
+  msgs!:Array<string>
+  
   ngOnInit(): void {
+    this.msgs = ['str','str2'];
   }
 
 }
