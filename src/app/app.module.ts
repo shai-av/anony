1bb06f40d7e071316b0119103404cbef0d2172f9
Shai <shay4447@gmail.com>
2022-09-17 18:29:02 +0300

    create chat-room component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,12 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { ChatRoomComponent } from './pages/chat-room/chat-room.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    ChatRoomComponent
   ],
   imports: [
     BrowserModule,
