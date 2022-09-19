a374e92ea18a4f04c41ebb41d6c9a15a40594396
Shai <shay4447@gmail.com>
2022-09-19 20:26:42 +0300

    create home page

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,11 +5,13 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { ChatRoomComponent } from './pages/chat-room/chat-room.component';
+import { HomeComponent } from './pages/home/home.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    ChatRoomComponent
+    ChatRoomComponent,
+    HomeComponent
   ],
   imports: [
     BrowserModule,
--- src/app/pages/chat-room/chat-room.component.ts
+++ src/app/pages/chat-room/chat-room.component.ts
@@ -1,5 +1,4 @@
-import { Component, OnInit } from '@angular/core';
-import { FormsModule } from '@angular/forms';
+import { Component, ElementRef, OnInit, ViewChild } from '@angular/core';
 
 @Component({
   selector: 'chat-room',
@@ -7,23 +6,40 @@ import { FormsModule } from '@angular/forms';
   styleUrls: ['./chat-room.component.scss']
 })
 export class ChatRoomComponent implements OnInit {
+  @ViewChild('chat')
+  chat!: ElementRef;
   
   constructor() { }
   
   msgs!:Array<any>
   msg:String = ''
-  userId:String = '12345'
+  userId:String = this._makeId()
+  
 
   ngOnInit(): void {
-    this.msgs = [{txt:'str',sentBy:'sad'},{txt:'str2',sentBy:'12345'}];
+    this.msgs = [{txt:'str',sentBy:'sad'},{txt:this.userId,sentBy:this.userId}];
   }
 
   sendMsg(){
     if(!this.msg) return
-    this.msgs.push({
+    this.msgs.unshift({
       txt:this.msg,
       sentBy:this.userId,
     })
     this.msg = ''
+    this._scrollToBottom()
+  }
+
+  _scrollToBottom(): void {
+        this.chat.nativeElement.scrollTop = this.chat.nativeElement.scrollHeight;
+}
+
+  _makeId(length=10){
+      var txt = '';
+      var possible = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789!@#$%^&*()_+?/|';
+      for (var i = 0; i < length; i++) {
+          txt += possible.charAt(Math.floor(Math.random() * possible.length));
+      }
+      return txt;
   }
 }
