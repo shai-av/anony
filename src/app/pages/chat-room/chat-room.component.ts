41165dd9c36ced27d928fee34b9d9272eacf4c6e
Shai <shay4447@gmail.com>
2022-11-09 14:54:43 +0200

    auto dlt msg after 5 sec

--- src/app/pages/chat-room/chat-room.component.ts
+++ src/app/pages/chat-room/chat-room.component.ts
@@ -8,39 +8,44 @@ import { Component, ElementRef, OnInit, ViewChild } from '@angular/core';
 export class ChatRoomComponent implements OnInit {
   @ViewChild('chat')
   chat!: ElementRef;
-  connectedUsersCount = 0 
-  
+  connectedUsersCount = 0
+
   constructor() { }
-  
-  msgs!:Array<any>
-  msg:String = ''
-  userId:String = this._makeId()
-  
+
+  msgs!: Array<any>
+  msg: String = ''
+  userId: String = this._makeId()
+
 
   ngOnInit(): void {
-    this.msgs = [{txt:'str',sentBy:'sad'},{txt:this.userId,sentBy:this.userId}];
+    // this.msgs = [{txt:'str',sentBy:'sad'},{txt:this.userId,sentBy:this.userId}];
+    this.msgs = [];
   }
 
-  sendMsg(){
-    if(!this.msg) return
+  sendMsg() {
+    if (!this.msg) return
     this.msgs.unshift({
-      txt:this.msg,
-      sentBy:this.userId,
+      txt: this.msg,
+      sentBy: this.userId,
     })
+
+    setTimeout(() => {
+      if (this.msgs.length) this.msgs.pop()
+    }, 5000)
     this.msg = ''
     this._scrollToBottom()
   }
 
   _scrollToBottom(): void {
-        this.chat.nativeElement.scrollTop = this.chat.nativeElement.scrollHeight;
-}
+    this.chat.nativeElement.scrollTop = this.chat.nativeElement.scrollHeight;
+  }
 
-  _makeId(length=10){
-      var txt = '';
-      var possible = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789!@#$%^&*()_+?/|';
-      for (var i = 0; i < length; i++) {
-          txt += possible.charAt(Math.floor(Math.random() * possible.length));
-      }
-      return txt;
+  _makeId(length = 10) {
+    var txt = '';
+    var possible = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789!@#$%^&*()_+?/|';
+    for (var i = 0; i < length; i++) {
+      txt += possible.charAt(Math.floor(Math.random() * possible.length));
+    }
+    return txt;
   }
 }
